4294408063aef2c40663d140fa760f8abb0d82fa
Daniel Shemesh <ds8200@gmail.com>
2023-09-28 11:08:09 +0300

    first commit

---
+++ src/components/RegularForm.tsx
@@ -0,0 +1,67 @@
+import { useForm } from 'react-hook-form';
+
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+function RegularForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(JSON.stringify(formData));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Change Me To React Hook Form</h1>
+      <div>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder='Enter UserName'
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder='Enter Email'
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          placeholder='Enter Password'
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default RegularForm;
