8f2ec7c977510b047e1b0d7a8bcbf2d7a811fe81
Daniel Shemesh <ds8200@gmail.com>
2023-09-28 12:05:36 +0300

    update validtion

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -7,10 +7,10 @@ interface FormData {
 }
 
 function RegularForm() {
-  const { 
+  const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors }
   } = useForm<FormData>();
 
   const onSubmit = (data: FormData) => {
@@ -22,30 +22,45 @@ function RegularForm() {
       <h1>Change Me To React Hook Form</h1>
       <div>
         <input
-          {...register("username", { required: true })}
+          {...register("username", {
+            required: true,
+            minLength: 2
+          })}
           type="text"
           id="username"
           placeholder="Enter UserName"
         />
-        {errors.username && <p>This field is required</p>}
+        {errors.username && <p>This field is required and must be at least 2 characters long.</p>}
       </div>
       <div>
         <input
-          {...register("email", { required: true })}
+          {...register("email", {
+            required: true,
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          })}
           type="text"
           id="email"
           placeholder="Enter Email"
         />
-        {errors.email && <p>This field is required</p>}
+        {errors.email && <p>This field is required and must be a valid email address.</p>}
       </div>
       <div>
         <input
-          {...register("password", { required: true })}
-          type="text"
+          {...register("password", {
+            required: true,
+            minLength: 8,
+            maxLength: 20, 
+            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).*$/,
+          })}
+          type="password"
           id="password"
           placeholder="Enter Password"
         />
-        {errors.password && <p>This field is required</p>}
+        {errors.password && (
+          <p>
+            This field is required and must meet the password requirements.
+          </p>
+        )}
       </div>
       <button type="submit">Submit</button>
     </form>
